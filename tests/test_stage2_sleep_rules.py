038d703c93aec8d54c26e5313277f72f6a069522
Camille Tran <Ctt011@ucsd.edu>
2026-03-07 21:39:52 -0800

    fixed

--- tests/test_stage2_sleep_rules.py
+++ tests/test_stage2_sleep_rules.py
@@ -12,7 +12,7 @@ import sys
 import os
 import pytest
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "src", "pipeline"))
 from stage2_sleep_rules import (
     SleepQualityAssessor,
     SleepAssessment,
